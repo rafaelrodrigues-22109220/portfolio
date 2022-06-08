2a187c7521b13730bd1eb59021f2919f1dde8f35
rafaelrodrigues-22109220 <a22109220@alunos.ulht.pt>
2022-06-08 12:12:41 +0100

    licenciatura fixed

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -7,6 +7,17 @@ class PostForm(ModelForm):
         model = Post
         fields = '__all__'
 
+class CadeiraForm(ModelForm):
+    class Meta:
+        model = Cadeira
+        fields = '__all__'
+
+
+class ProjetoForm(ModelForm):
+    class Meta:
+        model = Projeto
+        fields = '__all__'
+
 
         
 
