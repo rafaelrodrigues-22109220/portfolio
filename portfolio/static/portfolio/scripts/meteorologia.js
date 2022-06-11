24146bc2df2b09ba6d124beec53de74a8ee86337
rafaelrodrigues-22109220 <a22109220@alunos.ulht.pt>
2022-06-11 20:34:11 +0100

    temp

--- portfolio/static/portfolio/scripts/meteorologia.js
+++ portfolio/static/portfolio/scripts/meteorologia.js
@@ -1,7 +1,7 @@
-    var weatherSubmit = document.querySelector('.weatherSubmitCity')
     var maxTemp = document.querySelector('.maxTemp')
     var minTemp = document.querySelector('.minTemp')
     var dataTemp = document.querySelector('.data')
+
 
     window.onload = function(){
      fetch('//api.ipma.pt/open-data/forecast/meteorology/cities/daily/1110600.json')
@@ -10,6 +10,5 @@
             console.log(data);
             maxTemp.innerHTML = data['data'][0]['tMax'];
             minTemp.innerHTML = data['data'][0]['tMin'];
-            dataTemp.innerHTML = data['data'][0]['forecastDate'];
         })
     }
