1835db09f00753d26b2f9b2a1c4553ee62d03ec3
rafaelrodrigues-22109220 <a22109220@alunos.ulht.pt>
2022-06-11 21:27:27 +0100

    temp

--- portfolio/static/portfolio/scripts/meteorologia.js
+++ portfolio/static/portfolio/scripts/meteorologia.js
@@ -1,12 +1,20 @@
 
+    const sunny = [0,1]
+    const cloudy = [2, 3, 4, 5, 17, 24, 25, 26, 27, ]
+    const rain = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 23]
+    const snow = [18, 21, 22, 28, 29, 30]
 
     window.onload = function(){
         weather()
     };
 
     function weather(){
+
         var maxTemp = document.querySelector('.maxTemp')
         var minTemp = document.querySelector('.minTemp')
+
+        var weatherId = data['data'][0]['idWeatherType'];
+        var weatherIcon = "";
 
         fetch('//api.ipma.pt/open-data/forecast/meteorology/cities/daily/1110600.json')
             .then(response => response.json())
@@ -15,4 +23,19 @@
                 maxTemp.innerHTML = data['data'][0]['tMax'];
                 minTemp.innerHTML = data['data'][0]['tMin'];
             })
+
+
+
+
+        if (sunny.includes(weatherId)){
+            weatherIcon = "sun.png"
+        }else if(cloudy.includes(weatherId)){
+            weatherIcon = "cloud.png"
+        }else if(rain.includes(weatherId)){
+            weatherIcon = "rain.png"
+        }else if(snow.includes(weatherId)){
+            weatherIcon = "cloud.png"
+        }
+
+        document.getElementById("weather image").src = "portfolio/static/portfolio/images" + weatherIcon;
     }
