8360be9120d921fdec34b62e2184bf0e0790a982
rafaelrodrigues-22109220 <a22109220@alunos.ulht.pt>
2022-06-12 13:30:49 +0100

    temp

--- portfolio/static/portfolio/scripts/clock.js
+++ portfolio/static/portfolio/scripts/clock.js
@@ -19,7 +19,7 @@ function showTime(){
     s = (s < 10) ? "0" + s : s;
 
     var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
     document.getElementById("MyClockDisplay").textContent = time;
 
     setTimeout(showTime, 1000);
