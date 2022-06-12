7bd63f13e8eec2acd8dd8283305073db62b2cbb9
rafaelrodrigues-22109220 <a22109220@alunos.ulht.pt>
2022-06-12 13:54:47 +0100

    temp

--- portfolio/static/portfolio/scripts/clock.js
+++ portfolio/static/portfolio/scripts/clock.js
@@ -1,29 +1,24 @@
-function showTime(){
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
+function currentTime() {
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+  let session = "AM";
 
-    if(h == 0){
-        h = 12;
-    }
 
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
-    }
+  if(hh > 12){
+      session = "PM";
+   }
 
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
+   hh = (hh < 10) ? "0" + hh : hh;
+   mm = (mm < 10) ? "0" + mm : mm;
+   ss = (ss < 10) ? "0" + ss : ss;
 
-    const time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
+   let time = hh + ":" + mm + ":" + ss + " " + session;
 
-    setTimeout(showTime, 1000);
+  document.getElementById("clock").innerText = time;
+  var t = setTimeout(function(){ currentTime() }, 1000);
 
 }
 
-showTime();
+currentTime();
